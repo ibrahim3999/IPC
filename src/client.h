21027a82826f124812ed7d6f7938fb80290617bc
ibrahim3999 <mgdlhb@gmail.com>
2023-05-06 15:11:26 +0300

    h file

---
+++ src/client.h
@@ -0,0 +1,7 @@
+
+#ifndef CLIENT_H
+#define CLIENT_H
+
+void chat_client(char *ip_addr, int port);
+
+#endif
---
+++ src/server.h
@@ -0,0 +1,6 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+void chat_server(int port);
+
+#endif
