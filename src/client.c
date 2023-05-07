f948638bb307ee51fcdabe0a67fdaacf3b305e73
ibrahim3999 <mgdlhb@gmail.com>
2023-05-07 14:58:53 +0300

    poll()PARTA

--- src/client.c
+++ src/client.c
@@ -5,57 +5,70 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <poll.h>
 
 #define MAX_MESSAGE_LENGTH 1024
-#define SSA  (struct sockaddr *) 
+#define SSA  (struct sockaddr *)
 
-void chat_client(char *ip_addr, int port){
+void chat_client(char *ip_addr, int port) {
     int client_fd;
     struct sockaddr_in server_addr;
     char msg[MAX_MESSAGE_LENGTH];
 
-    client_fd = socket(AF_INET,SOCK_STREAM,0);
-    // create sokcet file descriptor
-    if(client_fd == -1){
+    // create socket file descriptor
+    if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         perror("Client -> socket failed");
         exit(EXIT_FAILURE);
     }
 
     // convert IP address to network order
-    if(inet_pton(AF_INET,ip_addr,&server_addr.sin_addr)<=0){
-       perror("Client -> inet_pton failed");
+    if (inet_pton(AF_INET, ip_addr, &server_addr.sin_addr) <= 0) {
+        perror("Client -> inet_pton failed");
         exit(EXIT_FAILURE);
     }
 
-    //set server addres and port
-     
-    memset (&server_addr, 0, sizeof(server_addr));
+    //set server address and port
+    memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(port);
 
-    if(connect(client_fd,SSA&server_addr,sizeof(server_addr))==-1){
-        perror("Client ->connect failed");
+    if (connect(client_fd, SSA &server_addr, sizeof(server_addr)) == -1) {
+        perror("Client -> connect failed");
         exit(EXIT_FAILURE);
     }
 
-    // send and receive msgs
-    while(1){
-        //send massage to server 
-        printf("Clinet: ");
-        fgets(msg,MAX_MESSAGE_LENGTH,stdin);
-        if(send(client_fd,msg,strlen(msg),0)<0){
-            perror("Client ->send falied");
+    //set up poll to listen to both socket and keyboard input
+    struct pollfd fds[2];
+    fds[0].fd = STDIN_FILENO;
+    fds[0].events = POLLIN;
+    fds[1].fd = client_fd;
+    fds[1].events = POLLIN;
+
+    //send and receive messages
+    while (1) {
+        if (poll(fds, 2, -1) == -1) {
+            perror("Client -> poll failed");
             exit(EXIT_FAILURE);
         }
 
-        //receive msg from server
-
-        if(recv(client_fd,msg,MAX_MESSAGE_LENGTH,0) < 0 ){
-            perror("Client ->recv falied");
-            exit(EXIT_FAILURE);
+        //if there is keyboard input, send it to server
+        if (fds[0].revents & POLLIN) {
+            printf("\nClient: ");
+            fgets(msg, MAX_MESSAGE_LENGTH, stdin);
+            if (send(client_fd, msg, strlen(msg), 0) == -1) {
+                perror("Client -> send failed");
+                exit(EXIT_FAILURE);
+            }
         }
-        printf("Server: %s",msg);
+
+        //if there is a message from server, print it to console
+        if (fds[1].revents & POLLIN) {
+            if (recv(client_fd, msg, MAX_MESSAGE_LENGTH, 0) <= 0) {
+                perror("Client -> recv failed");
+                exit(EXIT_FAILURE);
+            }
+            printf("\nServer: %s", msg);
+        }
     }
-
 }
--- src/server.c
+++ src/server.c
@@ -4,6 +4,8 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/poll.h>
 
 #define MAX_MESSAGE_LENGTH 1024
 #define SSA  (struct sockaddr *)
@@ -14,6 +16,7 @@ void chat_server(int port)
     struct sockaddr_in server_addr , client_addr;
     socklen_t client_addr_len = sizeof(client_addr);
     char msg[MAX_MESSAGE_LENGTH];
+    struct pollfd fds[2];
 
     // create socket file descriptor 
     server_fd = socket(AF_INET,SOCK_STREAM,0);
@@ -30,11 +33,11 @@ void chat_server(int port)
     }
 
     memset (&server_addr, 0, sizeof(server_addr));
-        server_addr.sin_family = AF_INET;
-        server_addr.sin_addr.s_addr = INADDR_ANY;
-        server_addr.sin_port = htons(port);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
+
     // bind socket to port 
-   
     if(bind(server_fd,SSA &server_addr,sizeof(server_addr))<0){
         perror("Server -> Falied bind ");
         exit(EXIT_FAILURE);
@@ -52,21 +55,38 @@ void chat_server(int port)
         exit(EXIT_FAILURE);
     }
 
+    // use poll to handle incoming messages from client and keyboard input
+    fds[0].fd = client_fd;
+    fds[0].events = POLLIN;
+    fds[1].fd = STDIN_FILENO;
+    fds[1].events = POLLIN;
+
     // send and receive messages
     while(1){
-        //recv message from clinet
-        if(recv(client_fd,msg ,MAX_MESSAGE_LENGTH,0)<0){
-            perror("Server ->  recv Falied ");
+        if(poll(fds,2,-1) < 0){
+            perror("Server -> Poll error");
             exit(EXIT_FAILURE);
         }
-        printf("Clinet: %s",msg);
 
-        //send msg to client 
-        printf("Server: ");
-        fgets(msg,MAX_MESSAGE_LENGTH,stdin);
-        if(send(client_fd,msg,strlen(msg),0)<0){
-            perror("Server ->  send Falied ");
-            exit(EXIT_FAILURE);
+        if(fds[0].revents & POLLIN){
+            //recv message from client
+            if(recv(client_fd,msg ,MAX_MESSAGE_LENGTH,0)<0){
+                perror("Server ->  recv Falied ");
+                exit(EXIT_FAILURE);
+            }
+            printf("Client: %s",msg);
+            strncpy(msg,"",1);
+        }
+
+        if(fds[1].revents & POLLIN){
+            // read message from keyboard
+            fgets(msg,MAX_MESSAGE_LENGTH,stdin);
+
+            //send message to client
+            if(send(client_fd,msg,strlen(msg),0)<0){
+                perror("Server ->  send Falied ");
+                exit(EXIT_FAILURE);
+            }
         }
     }
 
