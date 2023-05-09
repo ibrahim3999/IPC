a412749f40cb83f71327a45942b1a4041d8fce92
ibrahim mjadly <48316131+ibrahim3999@users.noreply.github.com>
2023-05-09 13:48:52 +0300

    Update server.c

--- src/server.c
+++ src/server.c
@@ -6,12 +6,21 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/poll.h>
+#include <netdb.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
 
 #define SSA  (struct sockaddr *)
 #define PORT 5777
 #define BUFFER_SIZE 1024
 #define LARGE_BUFFER_SIZE  104857600
 #define MAX_MESSAGE_LENGTH 1024
+#define UDS_PATH "/tmp/mysocket"
+
 
 void chat_server_TCP_IPV4(int port)
 {
@@ -94,6 +103,8 @@ void chat_server_TCP_IPV4(int port)
     }
    
 }
+
+
 //part B
 void start_server_TCP_IPv4() {
     int server_fd, new_socket, valread;
@@ -151,6 +162,8 @@ void start_server_TCP_IPv4() {
     close(new_socket);
     close(server_fd);
 }
+
+
 void start_server_TCP_IPv6() {
     int server_fd, new_socket, valread;
     struct sockaddr_in6 address;
@@ -207,8 +220,324 @@ void start_server_TCP_IPv6() {
     close(new_socket);
     close(server_fd);
 }
+void error(char *msg) {
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+void start_server_UDP_IPv4(int port) {
+    struct sockaddr_in server_addr, client_addr;
+    int sockfd, recv_len, total_recv = 0;
+    socklen_t client_len;
+    char buf[BUFFER_SIZE];
+
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
+        error("Error creating socket");
+    }
+
+    memset((char *)&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(port);
+
+    if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        error("Error binding socket");
+    }
+
+    printf("Server started on port %d...\n", port);
+
+    while (total_recv < 100000000) { // receive 100MB of data
+        memset(buf, 0, BUFFER_SIZE);
+        client_len = sizeof(client_addr);
+
+        if ((recv_len = recvfrom(sockfd, buf, BUFFER_SIZE, 0, (struct sockaddr *)&client_addr, &client_len)) == -1) {
+            error("Error receiving data");
+        }
+
+        if (sendto(sockfd, buf, recv_len, 0, (struct sockaddr *)&client_addr, client_len) == -1) {
+            error("Error sending data");
+        }
+
+        total_recv += recv_len;
+        printf("Received %d bytes of data\n", total_recv);
+
+    }
+
+
+    close(sockfd);
+}
+
+
+void start_server_UDP_IPv6(int port) {
+    struct sockaddr_in6 server_addr, client_addr;
+    int sockfd, recv_len, total_recv = 0;
+    socklen_t client_len;
+    char buf[BUFFER_SIZE];
+
+    if ((sockfd = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
+        error("Error creating socket");
+    }
+
+    memset((char *)&server_addr, 0, sizeof(server_addr));
+    server_addr.sin6_family = AF_INET6;
+    server_addr.sin6_addr = in6addr_any;
+    server_addr.sin6_port = htons(port);
+
+    if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        error("Error binding socket");
+    }
+
+    printf("Server started on port %d...\n", port);
+
+    while (total_recv < LARGE_BUFFER_SIZE) { // receive 100MB of data
+        memset(buf, 0, BUFFER_SIZE);
+        client_len = sizeof(client_addr);
+
+        if ((recv_len = recvfrom(sockfd, buf, BUFFER_SIZE, 0, (struct sockaddr *)&client_addr, &client_len)) == -1) {
+            error("Error receiving data");
+        }
+
+        char client_addr_str[INET6_ADDRSTRLEN];
+        inet_ntop(AF_INET6, &(client_addr.sin6_addr), client_addr_str, INET6_ADDRSTRLEN);
+
+        if (sendto(sockfd, buf, recv_len, 0, (struct sockaddr *)&client_addr, client_len) == -1) {
+            error("Error sending data");
+        }
+
+        total_recv += recv_len;
+        printf("Received %d bytes of data\n", total_recv);
+
+    }
+
+    close(sockfd);
+}
+
+//must check the function..
+void start_server_UDS_dgram() {
+    int server_fd, valread;
+    struct sockaddr_un address;
+    int opt = 1;
+    socklen_t addrlen = sizeof(address);
+    char *hello = "Hello from server";
+
+    // Create socket file descriptor
+    if ((server_fd = socket(AF_UNIX, SOCK_DGRAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set socket options
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        perror("setsockopt failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Bind socket to UDS path
+    memset(&address, 0, sizeof(address));
+    address.sun_family = AF_UNIX;
+    #define UDS_PATH "/tmp/my_unix_socket" //must define the relevant path
+    strncpy(address.sun_path, UDS_PATH, sizeof(address.sun_path) - 1);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Receive message from client
+    char buffer[BUFFER_SIZE] = {0};
+    int bytes_received = recvfrom(server_fd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&address, &addrlen);
+    if (bytes_received < 0) {
+        perror("recvfrom failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Received message from client: %s\n", buffer);
+
+    // Send response to client
+    int total_sent = 0;
+    while (total_sent < LARGE_BUFFER_SIZE) {
+        int bytes_sent = sendto(server_fd, hello, strlen(hello), 0, (struct sockaddr *)&address, addrlen);
+        if (bytes_sent < 0) {
+            perror("sendto failed");
+            exit(EXIT_FAILURE);
+        }
+        total_sent += bytes_sent;
+    }
+
+    printf("Sent %d bytes to client\n", total_sent);
+    close(server_fd);
+    unlink(UDS_PATH);
+}
+
+void start_server_UDS_stream() {
+    int server_fd, new_socket, valread;
+    struct sockaddr_un address;
+    int opt = 1;
+    socklen_t addrlen = sizeof(address);
+    char *hello = "Hello from server";
+
+    // Create socket file descriptor
+    if ((server_fd = socket(AF_UNIX, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set socket options
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        perror("setsockopt failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Bind socket to UDS path
+    memset(&address, 0, sizeof(address));
+    address.sun_family = AF_UNIX;
+    strncpy(address.sun_path, UDS_PATH, sizeof(address.sun_path) - 1);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen for connections
+    if (listen(server_fd, 3) < 0) {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Accept incoming connection
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen)) < 0) {
+        perror("accept failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Receive message from client
+    char buffer[BUFFER_SIZE] = {0};
+    int bytes_received = recv(new_socket, buffer, BUFFER_SIZE, 0);
+    if (bytes_received < 0) {
+        perror("recv failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Received message from client: %s\n", buffer);
+
+    // Send response to client
+    int total_sent = 0;
+    while (total_sent < LARGE_BUFFER_SIZE) {
+        int bytes_sent = send(new_socket, hello, strlen(hello), 0);
+        if (bytes_sent < 0) {
+            perror("send failed");
+            exit(EXIT_FAILURE);
+        }
+        total_sent += bytes_sent;
+    }
+
+    printf("Sent %d bytes to client\n", total_sent);
+    close(new_socket);
+    close(server_fd);
+    unlink(UDS_PATH);
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+#define FILENAME "example.txt" //change the file name.
+#define FILESIZE 4096
+
+void start_server_mmap() {
+    int fd;
+    char *mapped;
+
+    // Create file and memory map
+    if ((fd = open(FILENAME, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR)) == -1) {
+        perror("open failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (ftruncate(fd, FILESIZE) == -1) {
+        perror("ftruncate failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((mapped = mmap(NULL, FILESIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0)) == MAP_FAILED) {
+        perror("mmap failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Read from memory map
+    printf("Waiting for client to write to memory map...\n");
+    while (mapped[0] == '\0') {
+        usleep(1000);
+    }
+
+    printf("Received message from client: %s\n", mapped);
+
+    // Write to memory map
+    char *response = "Hello from server";
+    strncpy(mapped, response, strlen(response));
+
+    printf("Sent message to client: %s\n", mapped);
+
+    // Clean up
+    if (munmap(mapped, FILESIZE) == -1) {
+        perror("munmap failed");
+        exit(EXIT_FAILURE);
+    }
+
+    close(fd);
+    unlink(FILENAME);
+}
+
+void start_server_pipe() {
+    int fd;
+    char buffer[BUFFER_SIZE];
+
+    // Create named pipe
+    if (mkfifo(FILENAME, 0666) == -1) {
+        perror("mkfifo failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Open pipe for reading
+    if ((fd = open(FILENAME, O_RDONLY)) == -1) {
+        perror("open failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Read from pipe
+    int bytes_received = read(fd, buffer, BUFFER_SIZE);
+    if (bytes_received == -1) {
+        perror("read failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Received message from client: %s\n", buffer);
+
+    // Write to pipe
+    char *response = "Hello from server";
+    int bytes_sent = write(fd, response, strlen(response));
+    if (bytes_sent == -1) {
+        perror("write failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Sent message to client: %s\n");
+
+    // Clean up
+    close(fd);
+    unlink(FILENAME);
+}
+
+
 int main(){
     //start_server_TCP_IPv4();
-    start_server_TCP_IPv6();
+    //start_server_TCP_IPv6();
+    //start_server_UDP_IPv4(7777);
+    //start_server_UDP_IPv6(6666);
+
     return 0;
 }
