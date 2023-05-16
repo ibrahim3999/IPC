e2e04226ddb2986b868abea564dc870edda80dfd
ibrahim mjadly <48316131+ibrahim3999@users.noreply.github.com>
2023-05-16 03:07:49 +0300

    Update client.h

--- src/client.h
+++ src/client.h
@@ -3,13 +3,14 @@
 #define CLIENT_H
 
 void chat_client_TCP_IPv4(char *ip_addr, int port);
-void client_TCP_IPv4(char *ip_addr, int PORT) ;
+void client_TCP_IPv4(char *ip_addr, int PORT);
 unsigned int checksum(const char *buf, size_t len);
 void client_TCP_IPv6(char *ip_addr, int PORT);
 void client_UDP_IPv4(char *ip_addr, int PORT);
 void client_UDP_IPv6(char *ip_addr, int PORT);
+void client_UDS_dgram();
 void client_UDS_stream();
-void client_mmap() ;
+void client_mmap();
 void client_pipe();
 
 #endif
