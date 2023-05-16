f17f7bf950b6aa620e33a0ce2853d7820577d4bc
ibrahim mjadly <48316131+ibrahim3999@users.noreply.github.com>
2023-05-16 03:08:34 +0300

    Update server.h

--- src/server.h
+++ src/server.h
@@ -1,12 +1,9 @@
 #ifndef SERVER_H
 #define SERVER_H
-
-void chat_server(int port);
-void host_server(int,int);
 char* generate_data(int size);
 void chat_server_TCP_IPV4(int port);
+int host_server(int host_port,int port);
 unsigned int checksum_(const char *buf, size_t len);
-void start_server_TCP_IPv4(int PORT);
 void start_server_TCP_IPv6(int PORT);
 void start_server_UDP_IPv4(int PORT);
 void start_server_UDP_IPv6(int PORT);
@@ -14,7 +11,8 @@ void start_server_UDS_dgram();
 void start_server_UDS_stream();
 void start_server_mmap();
 void start_server_pipe();
-void host_server(int PORT ,int host_port);
+
+
 
 
 #endif
